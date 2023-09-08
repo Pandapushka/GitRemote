7a75caad2dbf65fee47b1a5b168908e88979dd78
andre <andreev.nik.d@yandex.ru>
2023-09-08 15:04:19 +0300

    Убрал лишнее

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -6,10 +6,7 @@ namespace ProjectForGit
     {
         static void Main(string[] args)
         {
-            //изменение для подзадачи1
-            //изменение для подзадачи2
-            //изменение для подзадачи3
-            //изменение для подзадачи4
+            
             Console.WriteLine("Hello World!");
         }
     }
